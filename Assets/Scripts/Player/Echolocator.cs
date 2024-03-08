16eaa676154fa8495c2905fc6118c5e2895c4dd1
matthewsunsailing <78941057+matthewsunsailing@users.noreply.github.com>
2024-03-08 10:29:37 -0800

    basic click show/hide

--- Assets/Scripts/Player/Echolocator.cs
+++ Assets/Scripts/Player/Echolocator.cs
@@ -24,6 +24,9 @@ public class Echolocator : MonoBehaviour
     private RaycastHit firstHit;
     private float firstHitDistance;
 
+    private bool isInsideTrigger;
+    private Collider trigger;
+
     void Start() {
         
     }
@@ -38,21 +41,47 @@ public class Echolocator : MonoBehaviour
 
         hits = Physics.RaycastAll(transform.position, tempDirection, range,
             detectableLayer, QueryTriggerInteraction.Collide);
-        
-        if (hits.Length > 0 ) {
+
+        if (hits.Length > 0)
+        {
             firstHit = hits[0];
-            firstHitDistance = firstHit.distance - 1.12f;
-            if (firstHitDistance < 0) {
+            firstHitDistance = firstHit.distance;
+            if (firstHitDistance < 0)
+            {
                 firstHitDistance = 0;
             }
 
             echoText.GetComponent<TextMeshProUGUI>().text =
-                firstHit.collider.gameObject.GetComponent<EchoMaterial>().materialText + ", "
+                firstHit.collider.gameObject.GetComponent<EchoMaterial>().materialText + "\n"
                 + firstHitDistance.ToString("F2") + "m";
 
             Debug.DrawLine(transform.position, firstHit.point, Color.yellow);
-        } else {
+        }
+        else if (isInsideTrigger)
+        {
+            echoText.GetComponent<TextMeshProUGUI>().text =
+                trigger.gameObject.GetComponent<EchoMaterial>().materialText + "\n" + "0.00m";
+        }
+        else
+        {
             echoText.GetComponent<TextMeshProUGUI>().text = "Out of Range";
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("detectable"))
+        {
+            trigger = other;
+            isInsideTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("detectable"))
+        {
+            trigger = null;
+            isInsideTrigger = false;
         }
     }
 }
