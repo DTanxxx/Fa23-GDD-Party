c03ada5068eb33ac80733e914c42b98a68b420e3
Nate1408 <nate1427@icloud.com>
2024-03-09 22:27:17 -0800

    final implementation

--- Assets/Scenes/Nathan/Hotbar.cs
+++ Assets/Scenes/Nathan/Hotbar.cs
@@ -3,37 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Hotbar : MonoBehaviour
 {
-    [SerializeField] Sprite[] hotbarSlot = new Sprite[5];
+    [SerializeField] Image[] hotbarSlot = new Image[5];
     [SerializeField] Sprite emptySprite;
-    [SerializeField] Sprite placeholder1;
-    [SerializeField] Sprite placeholder2;
+    [SerializeField] Sprite Plank; //will be set to placeholder sprite for now
+    [SerializeField] Sprite Rope;  //will be set to placeholder sprite for now
+    
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            if (hotbarSlot[0] == emptySprite)
+            foreach(Image image in hotbarSlot)
             {
-                hotbarSlot[0] = placeholder1;
+                if (image.sprite == emptySprite)
+                {
+                    image.sprite = Plank;
+                    break;
+                }
             }
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            if (hotbarSlot[1] == emptySprite)
+            foreach(Image image in hotbarSlot)
             {
-                hotbarSlot[1] = placeholder2;
+                if (image.sprite == emptySprite)
+                {
+                    image.sprite = Rope;
+                    break;
+                }
             }
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            hotbarSlot[0] = placeholder1;
+            for (int i = 0; i < hotbarSlot.Length; i++)
+            {
+                if (hotbarSlot[i].sprite == Plank)
+                {
+                    hotbarSlot[i].sprite = emptySprite;
+                    break;
+                }
+            }
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            hotbarSlot[1] = placeholder2;
+            for (int i = 0; i < hotbarSlot.Length; i++)
+            {
+                if (hotbarSlot[i].sprite == Rope)
+                {
+                    hotbarSlot[i].sprite = emptySprite;
+                    break;
+                }
+            }
         }
     }
 }
