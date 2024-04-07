afa8c030b6a2385df8a1cdc990567b01fc896432
ericxmccar <134644315+ericxmccar@users.noreply.github.com>
2024-04-07 14:37:01 -0700

    logic made for sprint

---
+++ Assets/Scripts/Player/PlayerSprinter.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+
+public class PlayerSprinter : MonoBehaviour
+{
+   
+    private float maxStamina = 100.0f;
+    private float currStamina;
+    public bool hasRegenerated = true;
+    public bool weAreSprinting = false; 
+
+    public static PlayerSprinter instance;
+    private float staminaRegen = 0.5f;
+    private float staminaDrain = 0.5f;
+    private float slowedRunSpeed = 0.1f;
+    private float normalRunSpeed = 0.2f;
+    private float sprintingRunSpeed = 0.3f; 
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        currStamina = maxStamina; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!weAreSprinting)
+        {
+            if (currStamina <= maxStamina - 0.01)
+            {
+                currStamina += staminaRegen * Time.deltaTime;
+
+                if (currStamina >= maxStamina)
+                {
+                    // set to normal speed
+                    hasRegenerated = true;
+                }
+            }
+        }
+    }
+
+    public void Sprinting()
+    {
+        if (hasRegenerated)
+        {
+            weAreSprinting = true;
+            currStamina -= staminaDrain * Time.deltaTime; 
+
+            if (currStamina <= 0)
+            {
+                hasRegenerated = false;
+                // set player speed to slowed 
+            }
+        }
+    }
+
+
+}
