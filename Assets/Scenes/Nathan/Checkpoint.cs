6ec592918234bcb2ce0dd51b2f4eee772a6c9e76
Nate1408 <nate1427@icloud.com>
2024-04-10 21:08:21 -0700

    skeleton

---
+++ Assets/Scenes/Nathan/Checkpoint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] private GameObject[] tileManagers;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
