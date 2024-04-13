e01f273460244a256c3b0c7fd7d7b057cd52d265
matthewsunsailing <78941057+matthewsunsailing@users.noreply.github.com>
2024-04-13 15:18:19 -0700

    functional dimmer

---
+++ Assets/Scripts/Player/LightDimmer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDimmer : MonoBehaviour
+{
+    [SerializeField] private float maxTime;
+    [SerializeField] private float minIntensity;
+
+    private float o_Intensity;
+    private Light flashlight;
+
+    private float currTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashlight = GetComponent<Light>();
+        o_Intensity = flashlight.intensity;
+        currTime = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currTime -= Time.deltaTime;
+
+        currTime = currTime < 0 ? 0 : currTime;
+
+        flashlight.intensity = currTime == 0 ? 0 : (currTime / maxTime) * (o_Intensity - minIntensity);
+    }
+
+    public void newBattery()
+    {
+        currTime = maxTime;
+        flashlight.intensity = o_Intensity;
+    }
+}
