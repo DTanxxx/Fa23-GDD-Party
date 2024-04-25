3de9960d58c6984069041ad58f14839f27fa40cb
matthewsunsailing <78941057+matthewsunsailing@users.noreply.github.com>
2024-04-24 20:53:33 -0700

    continue button/level saver implemented

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -18,6 +18,13 @@ namespace Lurkers.Control.Level
             if (SceneManager.GetActiveScene().buildIndex == 0)
             {
                 AudioManager.instance.Play(FMODEvents.instance.mainMenu, transform);
+
+                // if no currLevel PlayerPref key, set continue button to inactive
+                if (!PlayerPrefs.HasKey("currLevel"))
+                {
+                    GameObject continueButton = GameObject.Find("Continue Button");
+                    continueButton.SetActive(false);
+                }
             }
         }
 
@@ -25,7 +32,20 @@ namespace Lurkers.Control.Level
         public void LoadNextScene()
         {
             AudioManager.instance.StopAll();
+            // adds "currLevel" PlayerPref key, sets to next level
+            PlayerPrefs.SetInt("currLevel", SceneManager.GetActiveScene().buildIndex + 1);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        // button event
+        public void Continue()
+        {
+            // checks for existence of "currLevel" key
+
+            // if non-existent (via corruption, deletion, etc.),
+            // goes to level 1 (this shouldn't be possible, but is kept as a contingency)
+            int sceneIdx = PlayerPrefs.HasKey("currLevel") ? PlayerPrefs.GetInt("currLevel") : 1;
+            SceneManager.LoadScene(sceneIdx);
         }
 
         // button event
